bf559a02d479122197335c2aae9dcc5569ff9e4f
Ivan Litvinov <john.litvinov89@gmail.com>
2019-02-06 16:23:25 +0300

    Refactoring is in progress.

---
+++ BuroBeautyApp/BuroBeauty.DB.Entities/Dto/Master/MasterDayIncomeSummaryDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuroBeauty.DB.Entities.Dto.Master
+{
+    public class MasterDayIncomeSummaryDto
+    {
+        public class MasterIncomeSummaryListItem
+        {
+            public int Id { get; set; }
+            public string MasterFullName { get; set; }
+            public DateTime PurchaseDate { get; set; }
+            public decimal TotalAmount { get; set; }
+            public decimal Percent { get; set; }
+            public decimal AmountIncome { get; set; }
+        }
+    }
+}
--- BuroBeautyApp/BuroBeauty.Website/Controllers/CalculationOfMastersController.cs
+++ BuroBeautyApp/BuroBeauty.Website/Controllers/CalculationOfMastersController.cs
@@ -1,17 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
-
+using BuroBeauty.BLL;
+using BuroBeauty.BLL.Implementation;
+using BuroBeauty.DB.Entities;
+using BuroBeauty.Repositories;
+using BuroBeauty.Repositories.Implementation;
+using Microsoft.Ajax.Utilities;
 namespace BuroBeauty.Website.Controllers
 {
     public class CalculationOfMastersController : Controller
     {
+
+        private IOrderManager _orderManager;
+        private IServiceManager _serviceManager;
+        private IMasterManager _masterManager;
+
+        public CalculationOfMastersController()
+        {
+            IRepositorySettingsProvider settingsProvider = new RepositorySettingsProvider();
+            IOrderRepository orderRepository = new OrderRepository(settingsProvider);
+            _orderManager = new OrderManager(orderRepository);
+
+            IServiceRepository serviceRepository = new ServiceRepository(settingsProvider);
+            _serviceManager = new ServiceManager(serviceRepository);
+
+            IMasterRepository masterRepository = new MasterRepository(settingsProvider);
+            _masterManager = new MasterManager(masterRepository);
+        }
+
         // GET: CalculationOfMasters
         public ActionResult Index()
         {
-            return View();
+            // Подсчет суммы на опр.дату
+            var selectedDate = DateTime.Now;
+
+            var items = _orderManager.GetOrdersByDate(selectedDate);
+            ViewBag.Total = items.Sum(x => x.ServiceAmount);
+            ViewBag.SelectedDate = selectedDate;
+            return View(items);
+        }
+
+        [HttpPost]
+        public ActionResult Index(DateTime? date)
+        {
+            if (date == null)
+            {
+                return Index();
+            }
+
+            var items = _orderManager.GetOrdersByDate(date);
+            ViewBag.Total = items.Sum(x => x.ServiceAmount);
+            date = DateTime.Now;
+            ViewBag.SelectedDate = date;
+            return View(items);
         }
     }
 }
